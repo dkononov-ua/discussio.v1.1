2ba8d5e9012a1685c755b3954a925f1cbaa0cdc6
Denys <dkononov.ua@gmail.com>
2023-12-01 14:09:15 +0200

    3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,13 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs'
 
-// const httpsOptions = {
-//   key: fs.readFileSync('./secrets/privkey.pem'),
-//   cert: fs.readFileSync('./secrets/cert.pem'),
-// };
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/privkey.pem'),
+  cert: fs.readFileSync('./secrets/cert.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {httpsOptions});
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST'],
