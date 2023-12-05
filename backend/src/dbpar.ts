f3795a762eb874e032f809ef8f3954d493aa8e61
Denys <dkononov.ua@gmail.com>
2023-12-05 16:56:58 +0200

    228

--- backend/src/dbpar.ts
+++ backend/src/dbpar.ts
@@ -1,27 +1,26 @@
 /* eslint-disable prettier/prettier */
 
+const config = {
+    host: 'mysql',
+    user: 'root',
+    password: 'discuss32144',
+    insecureAuth: true,
+    database: "disscussio_beta_1",
+}
+
+
+
+
+
 // const config = {
-//     host: 'mysql',
+//     host: 'localhost',
+//     port: 3306,
 //     user: 'root',
 //     password: 'discuss32144',
 //     insecureAuth: true,
 //     database: "disscussio_beta_1",
 
 // }
-
-
-
-
-
-const config = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'discuss32144',
-    insecureAuth: true,
-    database: "disscussio_beta_1",
-
-}
 
 
 export default config
