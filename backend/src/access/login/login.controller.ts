4aade8a07251b77976c8c5133aa3c04b51893ad4
Denys <dkononov.ua@gmail.com>
2023-12-27 15:38:48 +0200

    123

--- backend/src/access/login/login.controller.ts
+++ backend/src/access/login/login.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Response, Body } from '@nestjs/common';
+import { Controller, Post, Response, Get, Body } from '@nestjs/common';
 import { LoginService } from './login.service';
 
 @Controller('login')
@@ -9,4 +9,10 @@ export class LoginController {
     async getLogin(@Response() res, @Body() tok:Object): Promise<any> {
       await this.loginService.getLogin( tok, res ); 
     }
+
+    @Get('first')
+    async getLoginFake(@Response() res, @Body() tok:Object): Promise<any> {
+      await this.loginService.getLogin( {email:"asdafd", password:"de12awd2"}, res ); 
+    }
+
 }
