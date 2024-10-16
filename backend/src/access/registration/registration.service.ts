bce54c928e10fb6cfedb3923502bf536fa36e704
Denys <dkononov.ua@gmail.com>
2024-10-16 22:29:10 +0300

    adsdf

--- backend/src/access/registration/registration.service.ts
+++ backend/src/access/registration/registration.service.ts
@@ -15,34 +15,39 @@ export class RegistrationService {
 
 
     async firstRegistration(inf: any, res: any): Promise<any> {
-      const startDate :any = new Date(inf.dob);
-      const endDate :any  = new Date()
-      const timeDifference = endDate - startDate;
-      const millisecondsInYear = 365.25 * 24 * 60 * 60 * 1000;
-      const yearsDifference = timeDifference / millisecondsInYear;
-      if(inf.regEmail !== undefined && inf.regEmail !== null && inf.regPassword !== undefined && inf.regPassword !== null && yearsDifference > 16){
-        let a = await this.Service.registercheck(inf.regEmail)
-        if(a){
-          res.status(200).json({ status: "Ви вже зареєстровані"});
+      try {
+        const startDate :any = new Date(inf.dob);
+        const endDate :any  = new Date()
+        const timeDifference = endDate - startDate;
+        const millisecondsInYear = 365.25 * 24 * 60 * 60 * 1000;
+        const yearsDifference = timeDifference / millisecondsInYear;
+        if(inf.regEmail !== undefined && inf.regEmail !== null && inf.regPassword !== undefined && inf.regPassword !== null && yearsDifference > 16){
+          let a = await this.Service.registercheck(inf.regEmail)
+          if(a){
+            res.status(200).json({ status: "Ви вже зареєстровані"});
+          }else{
+            const password = inf.regPassword;
+            const salt = '-vse23Discuss53213yt';
+            const hash = crypto.createHash('sha256').update(password + salt).digest('hex');
+            inf.regPassword = hash.slice(0, 30);  
+            let numb = await this.Service.new_secure(inf)
+            const html = `<p style="width: 400px; margin: 5px; color: gray; text-align: center; font-size: 16px;">Вас вітає служба безпеки</p>
+            <h1 style="width: 400px; margin: 5px; text-align: center; font-size: 40px; font-weight: 600; color: rgba(0, 0, 0, 0.678);">Discussio</h1>
+            <p style="width: 400px; margin: 5px; font-size: 20px; font-weight: 600; text-align: center; color: rgba(0, 0, 0, 0.678); ">Ваш код підтвердження: </p>
+            <p  style="width: 400px; margin: 5px; border: 1px solid gray; text-align: center; border-radius: 20px; font-size: 40px; font-weight: 600; padding: 10px; color: #ff6347;">${numb}</p>
+            <p style="width: 400px; margin: 5px; color: gray; text-align: center; font-size: 12px;">Якщо ви не робили ніяких дій на сайті Discussio  проігноруйте це повідомлення</p>
+            <p style="width: 400px; margin: 5px; color: gray; text-align: center; font-size: 10px;">© Discussio. Developed by Discussio team.</p>
+            `;
+            await sendMail(html, inf.regEmail)
+            res.status(200).json({ status: "На вашу пошту було надіслано код безпеки"});
+          }
         }else{
-          const password = inf.regPassword;
-          const salt = '-vse23Discuss53213yt';
-          const hash = crypto.createHash('sha256').update(password + salt).digest('hex');
-          inf.regPassword = hash.slice(0, 30);  
-          let numb = await this.Service.new_secure(inf)
-          const html = `<p style="width: 400px; margin: 5px; color: gray; text-align: center; font-size: 16px;">Вас вітає служба безпеки</p>
-          <h1 style="width: 400px; margin: 5px; text-align: center; font-size: 40px; font-weight: 600; color: rgba(0, 0, 0, 0.678);">Discussio</h1>
-          <p style="width: 400px; margin: 5px; font-size: 20px; font-weight: 600; text-align: center; color: rgba(0, 0, 0, 0.678); ">Ваш код підтвердження: </p>
-          <p  style="width: 400px; margin: 5px; border: 1px solid gray; text-align: center; border-radius: 20px; font-size: 40px; font-weight: 600; padding: 10px; color: #ff6347;">${numb}</p>
-          <p style="width: 400px; margin: 5px; color: gray; text-align: center; font-size: 12px;">Якщо ви не робили ніяких дій на сайті Discussio  проігноруйте це повідомлення</p>
-          <p style="width: 400px; margin: 5px; color: gray; text-align: center; font-size: 10px;">© Discussio. Developed by Discussio team.</p>
-          `;
-          await sendMail(html, inf.regEmail)
-          res.status(200).json({ status: "На вашу пошту було надіслано код безпеки"});
+            res.status(200).json({ status: "Ви не ввели почту або пароль"});
         }
-      }else{
-          res.status(200).json({ status: "Ви не ввели почту або пароль"});
+      } catch (error) {
+        res.status(200).json({ status: "Не валідна дата"});
       }
+
     }
 
     async finalRegistration(inf: any, res: any): Promise<any> {
@@ -99,35 +104,39 @@ export class RegistrationService {
 
 
     async forgotpass2(inf: any, res: any): Promise<any> {
-        
-        let secure = await this.Service.secure(inf.email)
-        if(secure){
-          let check = await this.Service.secure_check(inf.email, inf.passCode)
-          if(check.attempt_counter < 5){
-            const conee = mysql.createConnection(config)
-            try{
-              const password = inf.password;
-              const salt = '-vse23Discuss53213yt';
-              const hash = crypto.createHash('sha256').update(password + salt).digest('hex');
-              inf.password = hash.slice(0, 30); 
-              conee.query(
-                'UPDATE users SET password = ? WHERE user_mail = ?;',
-                // eslint-disable-next-line prettier/prettier
-                [inf.password, check.email],
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                async (err, result:any) => {
-                  // Додати до дениса
-                  res.status(200).json({ status: true, email: check.email, password: inf.password});
-                },
-              );
-            }catch(err){res.status(200).json({ status: "Не правильний код"});}finally{conee.end()} 
+        try {
+          let secure = await this.Service.secure(inf.email)
+          if(secure){
+            let check = await this.Service.secure_check(inf.email, inf.passCode)
+            if(check.attempt_counter < 5){
+              const conee = mysql.createConnection(config)
+              try{
+                const password = inf.password;
+                const salt = '-vse23Discuss53213yt';
+                const hash = crypto.createHash('sha256').update(password + salt).digest('hex');
+                inf.password = hash.slice(0, 30); 
+                conee.query(
+                  'UPDATE users SET password = ? WHERE user_mail = ?;',
+                  // eslint-disable-next-line prettier/prettier
+                  [inf.password, check.email],
+                  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                  async (err, result:any) => {
+                    // Додати до дениса
+                    res.status(200).json({ status: true, email: check.email, password: inf.password});
+                  },
+                );
+              }catch(err){res.status(200).json({ status: "Не правильний код"});}finally{conee.end()} 
+            }else{
+              await this.Service.updete_secure(check) 
+              res.status(200).json({ status: "Не правильний код"});
+            }
           }else{
-            await this.Service.updete_secure(check) 
-            res.status(200).json({ status: "Не правильний код"});
+            res.status(200).json({ status: false});
           }
-        }else{
-          res.status(200).json({ status: false});
+        } catch (error) {
+          res.status(200).json({ status: "Не валідний токен" });
         }
+
     }
 
 
